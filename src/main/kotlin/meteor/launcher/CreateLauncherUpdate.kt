2bebedc091cf9c6631e169a02798bf18bde6722f
TheRealNull <therealnull@gmail.com>
2022-04-19 20:20:45 -0400

    client: commit 1.2.4

--- src/main/kotlin/meteor/launcher/CreateLauncherUpdate.kt
+++ src/main/kotlin/meteor/launcher/CreateLauncherUpdate.kt
@@ -16,8 +16,8 @@ object CreateLauncherUpdate {
     @JvmStatic
     fun main(args: Array<String>) {
         val gson = GsonBuilder().setPrettyPrinting().create()
-        update.version = "1.2.3-3"
-        update.updateInfo = "ZGC, Events, and The Unethical Cuckening"
+        update.version = "1.2.3-4"
+        update.updateInfo = "small potatoes"
 
         if (releaseDir.exists())
             releaseDir.deleteRecursively()
