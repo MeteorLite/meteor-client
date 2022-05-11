9903a921e813aeb8a854e273040a994df734edbe
TheRealNull <therealnull@gmail.com>
2022-05-10 21:57:23 -0400

    [Automated]

--- src/main/kotlin/meteor/launcher/CreateLauncherUpdate.kt
+++ src/main/kotlin/meteor/launcher/CreateLauncherUpdate.kt
@@ -16,7 +16,7 @@ object CreateLauncherUpdate {
     @JvmStatic
     fun main(args: Array<String>) {
         val gson = GsonBuilder().setPrettyPrinting().create()
-        update.version = "1.4.2-1"
+        update.version = "1.4.3-2"
         update.updateInfo = "Items are okay I guess"
 
         if (releaseDir.exists())
