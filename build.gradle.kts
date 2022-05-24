3158342d8b982762bc8dfac2b7884c3946eb22c2
TheRealNull <therealnull@gmail.com>
2022-05-24 01:26:17 -0400

    [Automated]

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 val apiRelease by rootProject.extra { "1.4.8" }
-val release by rootProject.extra { "3" }
+val release by rootProject.extra { "4" }
 group = "meteor"
 version = "${apiRelease.split(".")[0]}.${apiRelease.split(".")[1]}${apiRelease.split(".")[2]}.$release"
 
