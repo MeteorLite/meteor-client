957b852d37bf9f8820d7fe4eb0b9505380cbf92e
TheRealNull <therealnull@gmail.com>
2022-09-15 15:11:37 -0400

    client: use api 1.6.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     `maven-publish`
 }
 
-val apiRelease by rootProject.extra { "1.6.8" }
+val apiRelease by rootProject.extra { "1.6.9" }
 val release by rootProject.extra { "1" }
 group = "meteor"
 version = "${apiRelease.split(".")[0]}.${apiRelease.split(".")[1]}${apiRelease.split(".")[2]}.$release"
