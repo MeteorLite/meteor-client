7dbc0038253e749eaa7e6d0023c3a272cfb07505
TheRealNull <therealnull@gmail.com>
2022-10-29 19:59:16 -0400

    client: revert plugin panel to working state

--- client/src/main/kotlin/meteor/ui/composables/PluginPanel.kt
+++ client/src/main/kotlin/meteor/ui/composables/PluginPanel.kt
@@ -1,40 +1,29 @@
 package meteor.ui.composables
 
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.dp
 import meteor.plugins.EventSubscriber
-import meteor.ui.composables.preferences.background
-import meteor.ui.composables.preferences.darkLightMode
 import meteor.ui.composables.preferences.darkThemeColors
-import meteor.ui.composables.preferences.lightThemeColors
 
 open class PluginPanel : EventSubscriber() {
     @Composable
     fun CreateComponent() {
-        val mod = Modifier.width(375.dp).height(2048.dp).background(background).padding(5.dp)
-
+        val mod = Modifier.background(darkThemeColors.background).fillMaxHeight()
 
         CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
-
-            LazyColumn(modifier =  mod) {
-                item {
-                    MaterialTheme(colors = if (darkLightMode.value) darkThemeColors else lightThemeColors) {
-                        Header()
-                        Content()
-                    }
+            Column(modifier = mod) {
+                MaterialTheme(colors = darkThemeColors) {
+                    Header()
+                    Content()
                 }
             }
-
         }
     }
 
