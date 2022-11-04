30caaddbf9535141d7402dcfd411758d2bf9ac15
TheRealNull <therealnull@gmail.com>
2022-11-04 15:14:59 -0400

    client: add driftnet

--- client/src/main/kotlin/meteor/plugins/PluginManager.kt
+++ client/src/main/kotlin/meteor/plugins/PluginManager.kt
@@ -67,6 +67,7 @@ import meteor.plugins.corp.CorpPlugin
 import meteor.plugins.exptracker.ExpTrackerPlugin
 import net.runelite.client.plugins.chatcommands.ChatCommandsPlugin
 import net.runelite.client.plugins.cluescrolls.ClueScrollPlugin
+import meteor.plugins.driftnet.DriftNetPlugin
 import net.runelite.client.plugins.gauntletextended.GauntletExtendedPlugin
 import net.runelite.client.plugins.herbiboars.HerbiboarPlugin
 import net.runelite.client.plugins.interfacestyles.InterfaceStylesPlugin
@@ -126,6 +127,7 @@ object PluginManager {
         init<CtrlPlayerOptions>()
         init<DefaultWorldPlugin>()
         init<DevToolsPlugin>()
+        init<DriftNetPlugin>()
         init<ExamplePlugin>()
         init<EntityHiderPlugin>()
         init<ExpTrackerPlugin>()
