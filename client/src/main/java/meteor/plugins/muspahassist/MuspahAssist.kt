3533a657dceb784993208be2fabfce2383fdb332
zeruth <therealnull@gmial.com>
2023-01-31 22:16:16 -0500

    client: add missing import

--- client/src/main/java/meteor/plugins/muspahassist/MuspahAssist.kt
+++ client/src/main/java/meteor/plugins/muspahassist/MuspahAssist.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
