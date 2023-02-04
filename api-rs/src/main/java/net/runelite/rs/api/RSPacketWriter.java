fc3a9a703fed562c82292cecc4b08a95da4ffe94
zeruth <therealnull@gmial.com>
2023-02-04 01:44:12 -0500

    client: remove garbage annotation in api-rs

--- api-rs/src/main/java/net/runelite/rs/api/RSPacketWriter.java
+++ api-rs/src/main/java/net/runelite/rs/api/RSPacketWriter.java
@@ -10,8 +10,6 @@ public interface RSPacketWriter extends PacketWriter
 	@Import("addNode")
 	void sendPacket(RSPacketBufferNode packet);
 
-	@Replace("addNode")
-
 	@Import("isaacCipher")
 	@Override
 	RSIsaacCipher getIsaacCipher();
